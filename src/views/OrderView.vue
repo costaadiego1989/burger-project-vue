0ae708bf28acb563f0227054103a6574c34b1514
Diego <anovaterradecristo@gmail.com>
2023-02-28 21:42:27 -0300

    feat: criado component de dashboard

--- src/views/OrderView.vue
+++ src/views/OrderView.vue
@@ -1,8 +1,20 @@
 <template>
   <div id="main-container">
     <h2>Página de Pedidos</h2>
+    <Dashboard />
   </div>
 </template>
+
+<script>
+import Dashboard from "../components/Dashboard.vue";
+
+export default {
+  name: "OrderView",
+  components: {
+    Dashboard,
+  }
+}
+</script>
 
 <style scoped>
 </style>
